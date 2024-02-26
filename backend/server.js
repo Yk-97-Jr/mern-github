05a27b20143cd97d49575d847815ff96a8f1cf51
youcef khalfi jr <youcefkhalfi5@gmail.com>
2024-02-26 22:48:55 +0100

    conect the db

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
@@ -19,4 +20,5 @@ app.use("/api/explore", exploreRoutes);
 
 app.listen(5000, () => {
   console.log("Server started on http://localhost:5000");
+  connectMongoDB();
 });
